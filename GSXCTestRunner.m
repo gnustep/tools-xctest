600d0019e58a5b15fc8914e37f428e837c29af76
Gregory John Casamento <greg.casamento@gmail.com>
2023-04-03 19:22:51 -0400

    Add license header to files

--- GSXCTestRunner.m
+++ GSXCTestRunner.m
@@ -5,6 +5,26 @@
 //  Created by Adam Fox on 9/17/18.
 //  Copyright © 2018 TestPlant, Inc. All rights reserved.
 //
+/*
+ This file is part of the GNUstep XCTEST Library.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; see the file COPYING.LIB.
+ If not, see <http://www.gnu.org/licenses/> or write to the
+ Free Software Foundation, 51 Franklin Street, Fifth Floor,
+ Boston, MA 02110-1301, USA.
+*/
+
 
 #import <GSXCTestRunner.h>
 #import <XCTest/XCTestCase.h>
